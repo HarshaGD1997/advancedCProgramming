ca2cd5ee6b123fbde518284254b0bd955ab53bf9
Harsha GD <harshagd695@gmail.com>
2021-08-29 16:48:16 +0530

    commit

--- chapter2/first.c
+++ chapter2/first.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
-void main(){
-	
+#define PI 3.14
 
+typedef int* int_pointer;
+
+void main(){
+	int a=12;
+	int_pointer b = &a;
+	printf("Value of int_pointer is %d\n",*b);
+	printf("address of a variable is %p\n",b);
 }
